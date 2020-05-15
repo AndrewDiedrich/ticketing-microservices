f0a50968e1b5cc2b27f1ecfa922891d6b8c85540
Andrew Diedrich <diedr067@gmail.com>
2020-05-14 20:53:27 -0600

    testing for new ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,9 +3,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 //middlewares
-import { errorHandler, NotFoundError } from '@andrewdied-tickets/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@andrewdied-tickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +20,11 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+// auth middlewares
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 // route not found error
 app.all('*', async (req, res) => {
