62e2a7beeb0ab4761ed6d5c2272b5b2fefc26bfb
Andrew Diedrich <diedr067@gmail.com>
2020-05-18 18:25:58 -0600

    adding nats pubsub to tickets service

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,21 +1,22 @@
-import nats, { Stan } from 'node-nats-streaming';
-
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: 'title123',
-    title: 'concert',
-    price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event Published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
