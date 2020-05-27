d5b443ea02ff89d149adb72832a0bdc392ffa527
Andrew Diedrich <diedr067@gmail.com>
2020-05-26 22:37:16 -0600

    mongo version plugin

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose, { mongo } from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@andrewdied-tickets/common';
 
 interface OrderAttrs {
@@ -44,6 +45,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
