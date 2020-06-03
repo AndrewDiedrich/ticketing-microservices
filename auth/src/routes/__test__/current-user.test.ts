e84d7e5cf89f2ee951b9e6a6fdaae9aebb569006
Andrew Diedrich <diedr067@gmail.com>
2020-06-03 15:22:33 -0600

    fix signin global

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-  const cookie = await global.signup();
+  const cookie = await global.signin();
   const response = await request(app)
     .get('/api/users/currentuser')
     //set cookie header to the cookie from the authResponse
